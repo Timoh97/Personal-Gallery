6db9ba1a96acc66f8feb980e163ee9a220f30cac
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-11-27 17:51:39 +0300

    add methods in models.py

--- myPics/models.py
+++ myPics/models.py
@@ -5,9 +5,14 @@ class Location(models.Model):
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =300)
     
+     def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length =30)
+    
+     def __str__(self):
+        return self.name
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
@@ -15,6 +20,9 @@ class Image(models.Model):
     description = models.CharField(max_length =300)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    
+     def __str__(self):
+        return self.name
     
     
     
