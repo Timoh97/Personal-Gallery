cb725b4234a3a54ce4c2544704a6596d14fcbc8f
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-11-27 18:03:19 +0300

    add content to welcome.html

--- myPics/models.py
+++ myPics/models.py
@@ -5,13 +5,13 @@ class Location(models.Model):
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =300)
     
-     def __str__(self):
+    def __str__(self):
         return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length =30)
     
-     def __str__(self):
+    def __str__(self):
         return self.name
 
 class Image(models.Model):
@@ -21,7 +21,7 @@ class Image(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     
-     def __str__(self):
+    def __str__(self):
         return self.name
     
     
--- myPics/views.py
+++ myPics/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render,redirect
 
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from . models import Image, Location, Category
 
 # Create your views here.
+
 def welcome(request):
-    return render(request, 'welcome.html')
-
-
-
-
+    title = 'Welcome to A++ Gallery'
+    images = Image.objects.all()
+    context={
+        'title': title,
+        'images' : images,
+    }
+    return render(request, 'welcome.html', context)
