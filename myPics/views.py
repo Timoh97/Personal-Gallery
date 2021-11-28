fd71665e5a47253410bb285b56fc1f118e7d84cb
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-11-28 11:44:51 +0300

    manipulate template variables and loops

--- myPics/views.py
+++ myPics/views.py
@@ -19,7 +19,7 @@ def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = Image.search_by_title(search_term)
+        searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"images": searched_images})
