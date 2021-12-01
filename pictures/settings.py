2f54a8482542cce381ab9a823e6a2bebcf9a18fa
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-01 10:00:54 +0300

    pushing to heroku

--- pictures/settings.py
+++ pictures/settings.py
@@ -18,9 +18,9 @@ from decouple import config,Csv
 
 MODE=config("MODE", default="prod")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False , cast=bool)
 # development
-if config('MODE')=="dev":
+if config('MODE')=="prod":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -56,7 +56,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qt@m8&m&xdmx^f8%%y+ei4*0pn5(+xur^&2e*!)#_!&kw=*&n!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
