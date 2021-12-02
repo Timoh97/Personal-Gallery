0003aace1c653505571f7994a0edf95f1d78ecf0
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-02 10:59:42 +0300

    add admin path

--- pictures/settings.py
+++ pictures/settings.py
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- pictures/urls.py
+++ pictures/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.urls import path
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
-    url(r'',include('myPics.urls'))
+    url(r'',include('myPics.urls')),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
